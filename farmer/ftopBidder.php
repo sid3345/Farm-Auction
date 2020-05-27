584aca8b6cece4c38dc7fe7ae3243c87877ab0ea
Siddharth Sinha <4117070-sid3345@users.noreply.gitlab.com>
2020-05-27 08:24:47 +0530

    trying pop up but :-1:  :(

--- farmer/ftopBidder.php
+++ farmer/ftopBidder.php
@@ -1,3 +1,5 @@
+<link href="../css/styles pop-up.css" rel="stylesheet"> 
+
 <?php include("../fheader.php");
 	if(isset($_SESSION["isLogedIn"]) && $_SESSION["isLogedIn"]==false){
 	?>
@@ -49,7 +51,77 @@
           
 		?>
       <tr>
-        <td style="cursor:pointer; color:#00008B; text-decoration:none;" onclick="location.href='userlist_popup.php?userName=<?php echo $row['userName'] ?>'"><?=$row["userName"]?> </td>
+      <script defer src="../js/script pop-up.js"></script>  
+      <body>
+      <td> <button data-modal-target="#modal"><?=$row["userName"]?></button> </td>
+      
+      <div class="modal" id="modal">
+        <div class="modal-header">
+          <div class="title">Example Modal</div>
+          <button data-close-button class="close-button">&times;</button>
+        </div>
+        <div class="modal-body">
+
+        Lorem ipsum dolor sit amet consectetur
+
+        ye bhi display nahi ho raha hay be box me.. :(
+        
+        agar box banke pop up aajaye toh fir ye neeche ka table box me daal sakte hay bc !!!  
+                    
+           <!-- <table class="table table-striped"> 
+              <thead>
+                <tr style="text-align-last: center;">
+                  <th>User name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Address</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody style="text-align-last: center;">
+
+              
+              <?php /*
+                  //checking user 
+                $check=false;
+                #$userName = $_GET['userName'];
+                $userName= $row["userName"];
+                $sql="SELECT name,email,phone,address, active FROM user WHERE admin=0 AND name='$userName'";
+                $result = $con->query( $sql );
+            
+              if ( $result->num_rows > 0 ) {
+                  foreach ( $result as $row1 ) {
+                
+              ?>
+                  <tr>
+                    <td><?=$row1["name"]?></td>
+                    <td><?=$row1["email"]?></td>
+                    <td><?=$row1["phone"]?></td>
+                    <td><?=$row1["address"]?></td>
+                          
+                    <?php 
+                  if($row1["active"]==1){
+                    #echo("Block");
+                    ?>
+                    <td><?="Blocked"?></td>
+                    <?php
+                    $check=true;
+                  }else{
+                    #echo("Unblock");
+                    ?>
+                    <td><?="Active"?></td>
+                    <?php
+                    $check=false;
+                  }
+                  ?></td>
+                </tr>
+                <?php } } */ ?>
+              </tbody>  -->
+          </div>
+        </div>
+        <div id="overlay"></div>
+        </body>
+      
         <td style="cursor:pointer; color:#00008B; text-decoration:none;" onclick="location.href='cropdetail.php?id=<?php echo $row['VID'] ?>'"><?=$row["name"]?></td>
         <td><?=$row["EndDate"]?></td>
         <td><img style="max-width: 200px; max-height: 200px;" src="<?=$_SESSION["directory"]?>img/vehicle/<?=$row["image"]?>" ></td>
