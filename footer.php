7050a8df69a6258d3c6dde781391c4c0a54cd348
Siddharth Sinha <4117070-sid3345@users.noreply.gitlab.com>
2020-05-29 14:46:50 +0530

    removed unnecessary social media links from footer

--- footer.php
+++ footer.php
@@ -22,9 +22,6 @@
                                 <ul class="social-icons social-icons-simple">
                                     <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                                     <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
                                 </ul>
                             </div>
                         </div>
