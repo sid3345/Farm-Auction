c950e38dcefdd9d7c61d4e2a9637e5dae33b9d7c
Siddharth Sinha <4117070-sid3345@users.noreply.gitlab.com>
2020-06-02 01:02:45 +0530

    unset session ID

--- logout.php
+++ logout.php
@@ -3,6 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();}
 unset($_SESSION["isLogedIn"]);
 unset($_SESSION["userid"]);
+unset($_SESSION["ID"]);
 $_SESSION["loginChk"]=0;
 header("location:login.php");
 ?>
