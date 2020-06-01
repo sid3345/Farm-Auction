6578e8b3c6d455fa75555f2b610232fad8d11b99
Siddharth Sinha <4117070-sid3345@users.noreply.gitlab.com>
2020-06-02 01:23:06 +0530

    added session destroy

--- logout.php
+++ logout.php
@@ -4,6 +4,9 @@ if (session_status() == PHP_SESSION_NONE) {
 unset($_SESSION["isLogedIn"]);
 unset($_SESSION["userid"]);
 unset($_SESSION["ID"]);
+
+session_destroy();
+
 $_SESSION["loginChk"]=0;
 header("location:login.php");
 ?>
