7e353752e083316ea5aa7ed140e97dffce87c4b3
Siddharth Sinha <4117070-sid3345@users.noreply.gitlab.com>
2020-06-22 21:28:36 +0530

    crop location marker for admin v1

--- admin/report.php
+++ admin/report.php
@@ -204,8 +204,6 @@ chart.render();
 </head>
 <body>
 <div class="container">
-	
-
 
 
 <?php
@@ -274,77 +272,303 @@ $Product3 = [];
 
 </div>
 
- <script>
+<script>
 
-		 function createChart(){
-			 var checkdate=$("#checkbid").val();
-			 $("#check").val(checkdate);
-			 $("#formpost").submit();
-			 
-		 }
-		 var chart = new CanvasJS.Chart("chart2", {
-	animationEnabled: true,
-	theme: "light2",
-	title:{
-		text: "Top bidding price of crop <?php if(!isset($_POST["hidName"])){ echo("(Last 7 days )");} ?> " 
-	},
-	axisY: {
-		title: "Price (INR)"
-	},
-	data: [{
-		type: "column",
-		yValueFormatString: "#,##0.##",
-		dataPoints: <?php echo json_encode($Product2, JSON_NUMERIC_CHECK); ?>
-	}]
+function createChart(){
+	var checkdate=$("#checkbid").val();
+	$("#check").val(checkdate);
+	$("#formpost").submit();
+	
+}
+var chart = new CanvasJS.Chart("chart2", {
+animationEnabled: true,
+theme: "light2",
+title:{
+	text: "Top bidding price of crop <?php if(!isset($_POST["hidName"])){ echo("(Last 7 days )");} ?> " 
+},
+axisY: {
+	title: "Price (INR)"
+},
+data: [{
+	type: "column",
+	yValueFormatString: "#,##0.##",
+	dataPoints: <?php echo json_encode($Product2, JSON_NUMERIC_CHECK); ?>
+}]
 });
 chart.render();
 
 		   
 		   
-		   
-		   
-		   
 //revenue
-	var chart2 = new CanvasJS.Chart("revenueChart", {
-	animationEnabled: true,
-	title:{
-		text: "Revenue Chart of Crop Auction"
-	},
-	axisY: {
-		title: "Revenue (in INR)",
-		 
-		suffix:  "INR"
-	},
-	data: [{
-		type: "column",
-		yValueFormatString: "#,##0 INR",
-		indexLabel: "{y}",
-		indexLabelPlacement: "inside",
-		indexLabelFontWeight: "bolder",
-		indexLabelFontColor: "white",
-		dataPoints: <?php echo json_encode($Product3, JSON_NUMERIC_CHECK); ?>
-	}]
+var chart2 = new CanvasJS.Chart("revenueChart", {
+animationEnabled: true,
+title:{
+	text: "Revenue Chart of Crop Auction"
+},
+axisY: {
+	title: "Revenue (in INR)",
+		
+	suffix:  "INR"
+},
+data: [{
+	type: "column",
+	yValueFormatString: "#,##0 INR",
+	indexLabel: "{y}",
+	indexLabelPlacement: "inside",
+	indexLabelFontWeight: "bolder",
+	indexLabelFontColor: "white",
+	dataPoints: <?php echo json_encode($Product3, JSON_NUMERIC_CHECK); ?>
+}]
 });
  
 chart2.render();
 </script>  
 
+<?php
+$sqlR="SELECT Region FROM vehicledetails";
+	
+$resultR=$con->query($sqlR);
+$resultF = array();
+while ($row = $resultR->fetch_array()) {
+	//$resultF[] = $row[0];
+	array_push($resultF,$row[0]);
+}
+//print_r($resultF);
+?>
+
 <!--Map-->
-  <h1 style="text-align-last: center;">Farmers Location</h1>
+  <h1 style="text-align-last: center;">Crop Location</h1>
 
 <script src="https://apis.mapmyindia.com/advancedmaps/v1/zmu2tzu3bz6ltcjewcfdd5xaagcw8agj/map_load?v=1.3"></script>
-<style> html, body, #map {margin: 0;padding: 0;width: 100%;height: 100%;} </style>
+
+<script type="text/javascript" src="../js/jquery.min_map_admin.js"></script>
+
+<style type="text/css"> html, body, #map {margin: 0;padding: 0;width: 100%;height: 100%;}             
+	#result {
+		position: absolute;left: 2px; top: 46px;width: 306px;bottom: 2px; border: 1px solid #cccccc;background-color: #FAFAFA; overflow: auto;
+	}
+
+	.event-header{
+		padding:14px 12px 6px 38px;color: #666;
+	}
+	#event-log{
+		padding:6px 12px 6px 38px;color: #777; font-size: 12px; line-height: 22px;
+	}
+	.map_marker{
+		position:relative;width:34px;height:48px
+	}
+	/*marker text span css*/
+	.my-div-span{
+		position: absolute;left:1.5em;right: 1em;top:1.4em;bottom:2.5em;font-size:9px;font-weight:bold;width:1px;color:black;
+	}
+</style>
+
 <div id="map"></div>;
-<script>	var map=new MapmyIndia.Map("map",{ center:[19.2856,72.8691],zoomControl: true,hybrid:true });	</script>
+<div class="event-header">Event Logs</div>
+<div id="event-log"></div>
+			
+<script type="text/javascript">
+var lng = 77.229455;
+var lat = 28.612960;
+var url_result;
+var all_result = [];
+var markerGroup = new Array();
+var markers = new Array();
+var map;
+var fieldList = ["eLoc", "geocodeLevel", "houseNumber", "houseName", "poi", "street", "subSubLocality", "subLocality", "locality", "subDistrict",
+	"district", "city", "state", "pincode", "formattedAddress"];
+var labelList = ["eLoc", "Geocode Level", "House Number", "House Name", "POI", "Street", "Sub Sub Locality", "Sub Locality", "Locality", "Sub District",
+	"District", "City", "State", "Pincode", "Address"];
+/*
+	1.Create a MapmyIndia Map by simply calling new MapmyIndia.Map() and passsing it at the minimum div object, all others are optional...
+	2.All leaflet mapping functions can be called simply on the L object
+	3.MapmyIndia may extend and in future modify the customised/forked Leaflet object to enhance mapping functionality for developers,
+	which will be clearly documented in the MapmyIndia API documentation section.
+	*/
+window.onload = function () {
+	var centre = new L.LatLng(lat, lng);
+	map = new MapmyIndia.Map('map', {center: centre, zoomControl: true, hybrid: true});
+	get_geocode_result();
+};
 
+/*function to get geocode result from the url*/
+function get_geocode_result() {
+		//var search_id = document.getElementById('search');
+		//var itemCount = parseInt(document.getElementById('itemcount').value);
+		var address_array =<?php echo json_encode($resultF)?>;
+
+		for (i = 0; i < address_array.length; i++) {
+			address= address_array[i];
+			//console.log('address: ',address);
+
+		/* if (address !== undefined && address.trim().length === 0) {
+			alert("Please enter address.");
+			search_id.focus();
+			return false;
+		} 
+		if (!validateInput(itemCount)) {
+			alert("Item count can be between 1 and 10.");
+			document.getElementById('itemcount').focus();
+			return false;
+		}
+		document.getElementById('result').innerHTML = '<div style="padding: 0 12px; color: #777">Loading..</div>'; //update best result 
+		document.getElementById('otherresult').innerHTML = '<div style="padding: 0 12px; color: #777">Loading..</div>'; /*update other result */
+		
+		getUrlResult(address, itemCount=1); /*get JSON resp*/
+	}
+}
+	/*
+	function validateInput(input) {
+		var flag = true;
+		if (input < 1 || input > 10) {
+			flag = false;
+		}
+		return flag;
+	}*/
+
+/*function to get Json response from the url*/
+function getUrlResult(address, itemCount) {
+	$.ajax({
+		type: "GET",
+		dataType: 'text',
+		url: "getResponseGeocode.php",
+		async: false,
+		data: {
+			address: address,
+			itemCount: itemCount
+		},
+		success: function (result) {
+			if (result !== undefined) {
+				var resdata = JSON.parse(result);
+				var copResults;
+				if (resdata.status === 'success') {
+					var jsondata = JSON.parse(resdata.data);
+					copResults = jsondata.copResults;
+					if (!Array.isArray(copResults)) {
+						copResults = Array.from(Object.keys(jsondata), k => jsondata[k])
+					}
+					if (copResults.length > 0) {
+						display_geocode_result(copResults); /*display results on success*/
+					}
+				} else {
+					hideLoader();
+					var error_response = "No result."
+					//document.getElementById('result').innerHTML = error_response + '</ul></div>'; /***put response result in div****/
+					//document.getElementById('otherresult').innerHTML = "" + '</ul></div>'; /***put response result in div****/
+				}
+			} else {
+				/*handle the error codes and put the responses in divs more error codes can be seen in the documentation*/
+				var error_response = "No result."
+				//document.getElementById('result').innerHTML = error_response + '</ul></div>'; /***put response result in div****/
+				//document.getElementById('otherresult').innerHTML = "" + '</ul></div>'; /***put response result in div****/
+			}
+		}
+	});
+}
+/*function to display geocode result*/
+function display_geocode_result(data) {
+
+	details = [];
+	/***********remove existing marker from map***********/
+	//remove_markers();
+	//var result_string = '<div style="padding: 0 12px;color:green;font-size:13px">Best Match</div><div style="font-size: 13px"><ul style="list-style-type:decimal;color:green; padding:2px 2px 0 30px">';
+	//var other_result_string = '<div style="padding: 0 12px;color:green;font-size:13px">Others</div><div style="font-size: 13px"><ul style="list-style-type:decimal; padding:2px 2px 0 30px">';
+	var num = 1;
+	var item = data[0];
+	var otheritem = data;
+	/*show the best item data*/
+	if (item !== '' && item !== null && item !== "undefined") {
+		var lng = item.longitude;
+		var lat = item.latitude;
+		var address = item.formattedAddress;
+		var pos = new L.LatLng(lat, lng); /***position of marker*****/
+		show_markers(num, pos, address); /**display markers***/
+
+		details.push(createPopupContent(item));
+
+		//result_string += '<li onclick="show_geocode_details(' + (num++) + ',' + lng + ',' + lat + ')">' + address + '</li>';
+	} 
+
+	mapmyindia_fit_markers_into_bound(); /***fit map in marker area***/
+}
+
+function createPopupContent(item) {
+	var content = new Array();
+	for (var i = 0; i < fieldList.length; i++) {
+		if (item[fieldList[i]] !== "") {
+			content.push("<tr><th style='white-space:nowrap;vertical-align: top;text-align: left;'>");
+			content.push(labelList[i]);
+			content.push("</th><th style='width:5px;vertical-align: top;text-align: left;'>:</th><td>");
+			content.push(item[fieldList[i]].trim().split(";")[0]);
+			content.push("</td></tr>");
+		}
+	}
+	return content.join("");
+}
+
+function show_markers(num, pos) {
+	var icon_marker = L.divIcon({
+		className: 'my-div-icon',
+		html: "<img style='position:relative;' \n\
+										src=" + "'https://maps.mapmyindia.com/images/2.png'>" + '<span style="position: absolute;left:1.6em;right:\n\
+										1em;top:1.4em;bottom:3.5em; font-size:9px;font-weight:bold;width: 1px; color:black;" class="my-div-span">' + num + '</span>',
+		iconSize: [10, 10],
+		popupAnchor: [12, -10]
+	}); /*creating a div icon*/
+
+	markers[num] = L.marker(pos, {icon: icon_marker}).addTo(map);
+	markers[num].bindPopup('<table style="line-height:14px" cellspacing="5">' + details[num] + '</table>');
+	//var popup = markers[num].getPopup();
+	//popup.setContent('<table style="line-height:14px" cellspacing="5">' + details[num] + '</table>').update();
+
+	markers[num].on("click", function (num) 
+	{ 	
+		markers[num].openPopup();
+	});	
+	map.setView(pos, 12);
+	markerGroup.push(markers[num]);
+}
+
+function show_geocode_details(num, lng, lat) {
+	show_info_window(num - 1, markers[num]);
+}
+
+/*function to show pop up on marker**/
+function show_info_window(num, marker) {
+	marker.openPopup();
+}
+
+/*function to remove  marker from the map*/
+function remove_markers() {
+	for (k = 0; k < markerGroup.length; k++) {
+		map.removeLayer(markerGroup[k]); /*Remove markers from map*/
+	}
+}
+
+/*function to fit the maps in the bounds of the marker*/
+function mapmyindia_fit_markers_into_bound() {
+	var group = new L.featureGroup(markerGroup);
+	map.fitBounds(group.getBounds());
+}
+
+function hideLoader() {
+	$("#loader").hide();
+}
+
+function showLoader() {
+	$("#loader").show();
+}
+
+
+</script>
 </body>
+
+<!-- Footer Section -->
+<?php
+
+include("../footer.php");
+?>
+
+<script type="text/javascript" src="../js/jquery.min_map_admin.js"></script>
+
 </html>       
-
- 
-  
-    	<!-- Footer Section -->
-	<?php
-
-	include("../footer.php");
-	?>D
-	<!-- Footer Section /- -->      
